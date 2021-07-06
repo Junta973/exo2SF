b6c97fa56af8a90bafc481f82d7d7917bd04bd84
medard <marc.medard973@gmail.com>
2021-07-06 12:05:49 +0200

    exo 2 twig

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoriesController extends AbstractController
+{
+private $categories = [
+    1 => [
+        "title" => "Politique",
+        "content" => "Tous les categorie liés à Jean Lassalle",
+        "id" => 1,
+        "published" => true,
+    ],
+    2 => [
+        "title" => "Economie",
+        "content" => "Les meilleurs tuyaux pour avoir DU FRIC",
+        "id" => 2,
+        "published" => true
+    ],
+    3 => [
+        "title" => "Securité",
+        "content" => "Attention les étrangers sont très méchants",
+        "id" => 3,
+        "published" => false
+    ],
+    4 => [
+        "title" => "Ecologie",
+        "content" => "Hummer <3",
+        "id" => 4,
+        "published" => true
+    ]
+    ];
+
+    /**Création de la première route*/
+    /**
+     * @Route ("/categories", name="categories")
+     */
+    public function categorieList()
+    {
+        return $this->render('categoriesList.html.twig', [
+            'categories' => $this->categories
+        ]);
+    }
+
+
+    /**Création de la seconde route pour afficher un seul article*/
+    /**
+     * @Route("/categorie/{id}", name="categorieShow")
+     */
+    public function categorieShow($id)
+    {
+        // j'utilise la méthode render de l'AbstractController
+        // pour récupérer un fichier Twig, le transformer en HTML
+        // et le renvoyer en réponse HTTP au navigateur
+        // Pour utiliser des variables dans le fichier twig, je dois
+        // lui passer un tableau en second parametre, avec toutes les
+        // variables que je veux utiliser
+        return $this->render('categorieShow.html.twig', [
+            'article' => $this->categories[$id]
+        ]);
+    }
+}
