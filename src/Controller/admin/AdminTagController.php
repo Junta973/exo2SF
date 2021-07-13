e86c3eb3306e69173e5dbac56e2f4aaa7ba3c080
medard <marc.medard973@gmail.com>
2021-07-13 13:54:07 +0200

    refactoring modification

--- src/Controller/admin/AdminTagController.php
+++ src/Controller/admin/AdminTagController.php
@@ -4,13 +4,58 @@
 namespace App\Controller\admin;
 
 
+use App\Entity\Tags;
 use App\Repository\TagsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminTagController extends AbstractController
 {
+
+    //Correspond au create du CRUD pour les Tags
+    /**
+     * @Route("/admin/tags/insert", name="admin_tag_insert")
+     */
+    public function createTag(EntityManagerInterface $entityManager,TagsRepository $tagsRepository)
+    {
+        // J'utilise l'entité Tags, pour créer un nouvel article en bdd
+        // une instance de l'entité Tags = un enregistrement d'article en bdd
+        $tag = new Tags();
+
+        // j'utilise les setters de l'entité Tags pour renseigner les valeurs des colonnes
+        $tag->setTitle('Black Friday');
+        $tag->setColor("black");
+
+        //Je sauvegarde les entitées crée
+        $entityManager->persist($tag);
+
+        // je récupère toutes les entités pré-sauvegardées et je les insère en BDD
+        $entityManager->flush();
+
+        return $this->redirectToRoute("admin_tags_list");
+
+    }
+
+
+    //Correspond a update du CRUD pour les Tags
+    /**
+     * @Route("/admin/tags/update/{id}", name="admin_tag_update")
+     */
+    public function updateTag($id,EntityManagerInterface $entityManager,TagsRepository $tagsRepository)
+    {
+            $tag = $tagsRepository->find($id);
+
+            $tag->setTitle('Tag modifier');
+
+            $entityManager->persist($tag);
+            $entityManager->flush();
+
+        return $this->redirectToRoute("admin_tags_list");
+    }
+
+
     /**
      * @Route("/admin/tags", name="admin_tags_list")
      */
@@ -48,4 +93,7 @@ class AdminTagController extends AbstractController
         ]);
 
     }
+
+
+
 }
